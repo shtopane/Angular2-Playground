fa130841edba7769d96eb82b4d30028677d6b561
kkirilova <vechesumstudent@gmail.com>
2017-09-08 12:49:14 +0300

    navbar ready, created components, position componenets in DOM

--- recepe-app/YummyDummy/src/app/app.module.ts
+++ recepe-app/YummyDummy/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabase} from 'angularfire2/database';
+
 import { AppComponent } from './app.component';
+
+import {environment} from '../environments/environment';
+
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'MandjaSGrozde')
   ],
-  providers: [],
+  providers: [AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- recepe-app/YummyDummy/src/environments/environment.ts
+++ recepe-app/YummyDummy/src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production:false,
+  firebase:{
+    apiKey: "AIzaSyBC9qYMwgt79BJ3yTnFWUg13PWNvRIYSYM",
+    authDomain: "yummydummy-a129e.firebaseapp.com",
+    databaseURL: "https://yummydummy-a129e.firebaseio.com",
+    projectId: "yummydummy-a129e",
+    storageBucket: "yummydummy-a129e.appspot.com",
+    messagingSenderId: "988509372190"
+  }
+  
 };
